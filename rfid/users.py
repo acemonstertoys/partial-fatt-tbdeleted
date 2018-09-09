e13ba0924dd6e4814670969613925749f7ca4de2
Elijah Verdoorn <everdoorn@pandora.com>
2018-09-08 17:28:53 -0700

    Add fetching of active RFIDs from WP
    Relies on a client secret from WP. Discuss access to this over slack.

--- rfid/users.py
+++ rfid/users.py
@@ -1,3 +1,5 @@
+import json
+import requests
 
 @app.route("/rfids/active")
 def get_active_rfids():
@@ -7,16 +9,17 @@ def get_active_rfids():
             the second being another array with each of the 10-digit RFID
             tags of the active members.
     """
-    rp=None
-    status="BAD"
-    if status is "OK":
-        rfids = []
-        data['rfids'] = r
-        rp=ApiResult(rfids, 200)
-    else:
-        raise ApiException('Oh no its bad')
+    rp = None
+    status = "BAD"
+    url = 'https://www.acemonstertoys.org/wp-json/amt/v1/rfids/active'
+    headers = {'X-Amt-Auth': get_secret()}
+    r = requests.get(url, headers=headers)
+    json = r.json()
+    rfids = json[1]
+    data['rfids'] = r
+    rp = ApiResult(rfids, 200)
+
     return rp
-#    return json.dumps([status, data])
 
 @app.route("/rfid/<int:tagID>/active")
 def get_rfid_status(tagID):
@@ -26,4 +29,10 @@ def get_rfid_status(tagID):
     :returns: True or False
     """
     return json.dumps(False)
+
+def get_secret():
+    file = open('../config.json', 'r')
+    data = json.load(file)
+    file.close()
+    return data['wp-api-secret']
 
